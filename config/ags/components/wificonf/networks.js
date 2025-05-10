8a2f16b06f8946e5dda9ec7acd3a461a4fea5fda
Qu4s4rM <cristianandresca23@gmail.com>
2025-05-10 16:15:14 -0500

    Update

--- config/ags/components/wificonf/networks.js
+++ config/ags/components/wificonf/networks.js
@@ -1,8 +1,8 @@
 export var networks = [
   {"id":1,"name":"FAMILIA SOTO-2.4G","bssid":"50:5B:1D:52:4F:F6","signal":62},
-  {"id":2,"name":"ERROR 404 ","bssid":"30:1F:48:A5:CA:2E","signal":60},
+  {"id":2,"name":"ERROR 404 ","bssid":"30:1F:48:A5:CA:2E","signal":59},
   {"id":3,"name":"CLAN-HIDALGO-2.4G","bssid":"80:F7:A6:85:D4:30","signal":45},
-  {"id":4,"name":"CLAN-HIDALGO-5G","bssid":"80:F7:A6:85:D4:2C","signal":44},
-  {"id":5,"name":"FAMILIA SOTO-5G","bssid":"50:5B:1D:52:4F:F2","signal":40},
-  {"id":6,"name":"Gomez-2.4G","bssid":"F4:2D:06:E1:03:42","signal":37}
+  {"id":4,"name":"CLAN-HIDALGO-5G","bssid":"80:F7:A6:85:D4:2C","signal":45},
+  {"id":5,"name":"CLAN-JEAMPI-2.4G","bssid":"80:F7:A6:86:58:FC","signal":44},
+  {"id":6,"name":"Gomez-2.4G","bssid":"F4:2D:06:E1:03:42","signal":39}
 ]
--- config/ags/scripts/change_theme.sh
+++ config/ags/scripts/change_theme.sh
@@ -15,7 +15,7 @@ SCSS_FILE="$HOME/.config/ags/scss/colors.scss"
 # Colores predefinidos para el modo oscuro
 DARK_BG="#131412"
 DARK_BG_BLUR="#18111abb"
-DARK_BG_WIDGETS="#242522"
+DARK_BG_WIDGETS="#1f272e"
 DARK_FG_WIDGETS="#3b3d38"
 DARK_BTN_WORK="#505050"
 DARK_BG_PANELS="#1B1C1A"
@@ -23,13 +23,13 @@ DARK_FG_PANELS="#262724"
 DARK_TEXT_COLOR="#D1D1D1"  # Color de texto en modo oscuro
 
 # Colores predefinidos para el modo claro
-LIGHT_BG="#ffffff"
+LIGHT_BG="#e7e7e7"
 LIGHT_BG_BLUR="#f0f0f0bb"
-LIGHT_BG_WIDGETS="#f4f4f4"
-LIGHT_FG_WIDGETS="#333333"
+LIGHT_BG_WIDGETS="#ffffff"
+LIGHT_FG_WIDGETS="#cccccc"
 LIGHT_BTN_WORK="#cccccc"
-LIGHT_BG_PANELS="#e1e1e1"
-LIGHT_FG_PANELS="#444444"
+LIGHT_BG_PANELS="#ffffff"
+LIGHT_FG_PANELS="#bdbdbd"
 LIGHT_TEXT_COLOR="#333333"  # Color de texto en modo claro
 
 # Generación del archivo SCSS según el modo
